d4b57fe15f7c7e829babb4ce6d11975a4c249f49
Scott Moen <valdarin@gmail.com>
2014-06-10 23:04:41 -0700

    completed assignment 1

---
+++ Matchismo/Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Scott Moen on 6/10/14.
+//  Copyright (c) 2014 Scott Moen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Scott Moen on 6/10/14.
+//  Copyright (c) 2014 Scott Moen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
