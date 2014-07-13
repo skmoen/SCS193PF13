c7495eb368d28d57b3c3a0f7301967a260c0d44a
Scott Moen <valdarin@gmail.com>
2014-07-12 21:44:20 -0700

    create new project

---
+++ TopPlaces/TopPlaces/TopPlacesViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopPlacesViewController.h
+//  TopPlaces
+//
+//  Created by Scott Moen on 7/12/14.
+//  Copyright (c) 2014 Scott Moen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopPlacesViewController : UIViewController
+
+@end
